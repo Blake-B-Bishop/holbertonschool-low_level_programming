57d7dc77357e2f20d72ef23b4ce7354acdeb7206
Blake-B-Bishop <blake_bishop@yahoo.com>
2023-01-26 13:53:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-	/**
-	* main - Entry point
-     	*
-        * Description: prints "Programming is like building a puzzle"
-	*
-	* Return: Always 0 (Success)
-	*/
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
