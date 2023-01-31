e00e7d82e3b1578c4254528b7b040df2e362d18b
Blake-B-Bishop <blake_bishop@yahoo.com>
2023-01-30 17:06:18 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: Always return 0
+ * Return: Always 0
  */
 int main(void)
 {
 	printf("_putchar\n");
-	return (0);
+	retun (0);
 }
