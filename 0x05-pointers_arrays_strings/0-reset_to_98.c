69dcf090bbc30732cbf039c5e227e090baef4729
Blake-B-Bishop <blake_bishop@yahoo.com>
2023-02-13 07:15:13 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Pointer to int & updates
+ * points to value of 98
+ *
+ * @n: integer pointer
+ *
+ * Return : nothing
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
