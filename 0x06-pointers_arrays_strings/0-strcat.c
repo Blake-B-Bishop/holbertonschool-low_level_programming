47427316d841835ba14b0ceeb84184f1d8474d20
Blake-B-Bishop <blake_bishop@yahoo.com>
2023-02-13 09:23:23 -0800

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Mixes two strings
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Pointer to resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
