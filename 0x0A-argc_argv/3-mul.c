38b31c48de676338d73d467fc36751e3359fedaa
Blake-B-Bishop <blake_bishop@yahoo.com>
2023-02-17 22:44:07 -0800

    carlos was here on thursday but not wednesday

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two number pulled from the argv numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: ITS STILL 0
+ */
+int main(int argc, char *argv[])
+{
+	int j1 = 0, j2 = 0;
+	
+	if (argc == 3)
+	{
+		j1 = atoi(argv[1]);
+		j2 = atoi(argv[2]);
+		printf("%d\n", j1 * j2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - adds two numbers
+ *@argc: arguement counter
+ *@argv: arguement vector
+ *Return: comes back 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, d, sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (d = 0; argv[i][d]; d++)
+		{
+			if (argv[i][d] < '0' || argv[i][d] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}	
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
